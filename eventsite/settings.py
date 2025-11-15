1898c6ce89db4785c31d75e1a9bfda5ea1381a7e
algo-sanya <sanya.arora@algoscale.com>
2025-11-16 01:24:43 +0530

    Update settings for Render

--- eventsite/settings.py
+++ eventsite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure--dif_yall*eboqm5j43^(bz+3fu9p0=hem&har2)xj@cwvic((
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
